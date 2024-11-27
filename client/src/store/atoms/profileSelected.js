3f87928ac7d3c25b818d61582152af5b95ab2afa
Akshay Srivastava <akshay.srivastava113@gmail.com>
2024-11-28 00:04:55 +0530

    Revert "Change tsconfig"
    This reverts commit eecb1f5695bdf696d019d687f7ae0b3ea872bf3e.

--- client/src/store/atoms/profileSelected.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.profileSelected = void 0;
-const recoil_1 = require("recoil");
-exports.profileSelected = (0, recoil_1.atom)({
-    key: 'profileSelected', // unique ID
-    default: false, // default value
-});
